21e15c8d4c21e5430d6a0f8afcaae5887cba5ed4
JorgeLuix <tec.jorge.29@gmai.com>
2023-03-12 10:42:14 +0100

    finish fizzbuzz

--- js/script.js
+++ js/script.js
@@ -2,16 +2,32 @@ const totalBox = 100;
 
 const gridd = document.getElementById('gridd');
 
-for (let i = 1; i <= totalBox; i++) {
-    const lightbox = (i % 2 === 0) ? 'light' : '';
-    /*if ((i % 3 === 0) && (i % 5 === 0)) {
-        yellowbox = 
-    }*/
-    gridd.innerHTML += 
+
+for (let square = 1; square <= totalBox; square++) {
+    let testo = square;
+    let colorbzz = '';
+    let colorfzz = '';
+    let colorfbuzz = '';
+    if ( square % 15 === 0 ) {
+        testo = 'fizzbuzz';
+        colorfbuzz = 'fbuzz';
+
+    }
+    else if ( square % 3 === 0 ) {
+        testo = 'fizz';
+        colorfzz = 'fzz';
+    }
+    else if (square % 5 === 0 ){
+        testo = 'buzz';
+       colorbzz = 'bzz';
+    }
+   
+     gridd.innerHTML += 
      `<div class="squaresmall ">
              <div class="contenuto d-flex justify-content-center 
-                align-items-center border rounded-2 ${lightbox}">
-                ${i} 
+                align-items-center  ${colorbzz} ${colorfzz} ${colorfbuzz}">
+                ${testo}
             </div>
         </div>`;
+   
  }
